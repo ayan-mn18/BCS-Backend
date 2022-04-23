339a20dc673a9a856d4c3b31bc990924b6967d87
Pushpendra Shukla <pushpendrashukla620@gmail.com>
2022-04-23 22:37:14 +0530

    added featured product apis

--- Controller/featured_product.controller.js
+++ Controller/featured_product.controller.js
@@ -27,12 +27,22 @@ const addFeaturedProduct=async (req,res,)=>{
         const product_id = req.params.pid
         
 
-        console.log(req.body)
+        
         const addedProduct=await Featured_product.create(req.body);
         console.log(addedProduct.id)
         const parentProduct = await Product.findByIdAndUpdate(product_id , {
             $push : {'featured_product_id' : addedProduct.id}
         });
+    
+        if(parentProduct==null){
+            errorMessage(
+                res,
+                "error",
+                error,
+    
+            );
+
+        }
 
         successMessage(
             res,
@@ -53,6 +63,131 @@ const addFeaturedProduct=async (req,res,)=>{
 
     }
 }
+
+
+const getFeaturedProductById=async (req,res,)=>{
+    
+    try{
+        const id = req.params.pid ;
+        //id validity check
+        
+
+        const gotproductById=await Product.find({'_id':id},{'featured_product_id':[req.params.fpid]})
+        if (!gotproductById.length) {
+            
+            return errorMessage(
+                res,
+                "Product not found!!",
+                gotproductById,
+    
+            );
+            
+        } 
+        const fid =req.params.fpid;
+        const gotFeaturedProductById= await Featured_product.findById(fid);
+        if(!gotFeaturedProductById){
+            errorMessage(
+                res,
+                "Product not found!!",
+                getFeaturedProductById,
+    
+            );
+        }
+        
+        
+        dataa={gotFeaturedProductById,gotproductById};
+        successMessage(
+            res,
+            "Product found",
+            data=dataa,
+        );
+    }
+    catch(error){
+        errorMessage(
+            res,
+            "Product not found!!",
+            error,
+
+        );
+
+    }
+};
+
+const updateFeaturedProductById=async (req,res,)=>{
+    // image upload multiple
+    
+    try{
+        const id=req.params.fpid;
+        // if(req.file.path){
+
+        // }
+        //CHECK PHOTO UPDATE CODE .
+        const updates=req.body;
+        const options={new:true}
+        const updatedProduct=await Featured_product.findByIdAndUpdate(id,updates,options);
+        if (!updatedProduct){
+            errorMessage(
+                res,
+                "Product not found!!",
+                data=updatedProduct
+    
+            );
+
+        }
+        
+        successMessage(
+            res,
+            "Product found",
+            updatedProduct,
+        );
+    }
+    catch(error){
+        errorMessage(
+            res,
+            "Product not found!!",
+            error,
+
+        );
+
+    }
+};
+
+const deleteProductById=async (req,res,)=>{
+    
+    try{
+        const product_id=req.params.pid;
+        const fid=req.params.fpid;
+        const delProductById=await featured_product_id.find({'_id':fid});
+        if (!delProductById) {
+            return res.status(404).json({ message: "Resource not found" });
+        }
+        const parentProduct = await Product.findByIdAndUpdate(product_id , {
+            $pull : {'featured_product_id' :fid}
+            
+        });
+        
+        successMessage(
+            res,
+            "Product found",
+            parentProduct,
+        );
+    }
+    catch(error){
+        errorMessage(
+            res,
+            "Product not found!!",
+            error,
+
+        );
+
+    }
+};
+
 module.exports={
-    addFeaturedProduct
+    addFeaturedProduct,
+    getFeaturedProductById,
+    updateFeaturedProductById,
+    deleteProductById,
+
+
 }
--- Routes/featured_product.routes.js
+++ Routes/featured_product.routes.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const { route } = require('express/lib/application');
 const { isAuthenticated, isAdmin } = require('../Config');
 const is_Admin = require('../Config/isAdmin.config');
-const { addFeaturedProduct } = require('../Controller/featured_product.controller');
+const { addFeaturedProduct, getFeaturedProductById, updateFeaturedProductById } = require('../Controller/featured_product.controller');
+const { deleteProductById } = require('../Controller/product.controller');
 const { upload } = require('../Utils/multer');
 
 const router = express.Router();
 
 router.post('/:pid' , isAuthenticated , isAdmin , addFeaturedProduct );
+router.get('/:pid/:fpid' , isAuthenticated , isAdmin , getFeaturedProductById );
+router.patch('/:pid/:fpid',isAuthenticated,isAdmin,updateFeaturedProductById);
+router.delete('/:pid/:fpid',isAuthenticated,isAdmin,deleteProductById);
+
 
 module.exports = router;
-
 
