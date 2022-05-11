029403ab08b5e13bdb1f0dd5e846db0389d05557
svnitad <svnitad@gmail.com>
2022-05-11 17:47:22 +0530

    azhar - isAuthenticated Removed

--- Routes/product.routes.js
+++ Routes/product.routes.js
@@ -1,23 +1,28 @@
-
-const express = require('express');
-const { isAuthenticated, isAdmin } = require('../Config');
-const is_Admin = require('../Config/isAdmin.config');
-const { getProduct, addProduct, getProductById, deleteProductById, updateProductById } = require("../Controller/product.controller");
-const cloudinary=require('../Utils/cloudinary');
-const { upload } = require('../Utils/multer');
+const express = require("express");
+const { isAuthenticated, isAdmin } = require("../Config");
+const is_Admin = require("../Config/isAdmin.config");
+const {
+  getProduct,
+  addProduct,
+  getProductById,
+  deleteProductById,
+  updateProductById,
+} = require("../Controller/product.controller");
+const cloudinary = require("../Utils/cloudinary");
+const { upload } = require("../Utils/multer");
 
 const router = express.Router();
 
-
-router.get('/getproduct',isAuthenticated,is_Admin,getProduct);
-router.get('/:pid',isAuthenticated,getProductById);
-router.post('/addproduct',isAuthenticated,is_Admin,addProduct);
-router.delete('/:pid',isAuthenticated,is_Admin,deleteProductById);
-router.patch('/:pid',isAuthenticated,is_Admin,upload.array('url'),updateProductById)
-
-
-
-
+router.get("/getproduct", getProduct);
+router.get("/:pid", getProductById);
+router.post("/addproduct", isAuthenticated, is_Admin, addProduct);
+router.delete("/:pid", isAuthenticated, is_Admin, deleteProductById);
+router.patch(
+  "/:pid",
+  isAuthenticated,
+  is_Admin,
+  upload.array("url"),
+  updateProductById
+);
 
 module.exports = router;
-
