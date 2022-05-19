38f7e1096c26e0c039fcf97b4c84796349fbcfeb
svnitad <svnitad@gmail.com>
2022-05-20 00:01:39 +0530

    initial commitaaaaasdd

--- configf/db.config.js
+++ configf/db.config.js
@@ -1,23 +1,23 @@
-const mongoose = require('mongoose');
-const colors = require('colors');
+const mongoose = require("mongoose");
+const colors = require("colors");
 
-const username = "Bizer44";
-const password = "sexyboltenabhai";
-const cluster = "cluster0.rzwrw";
+const username = "hbn_mongo";
+const password = "HbnSaddweb123";
+const cluster = "cluster0.jaud8";
 const dbname = "BCS";
 
 mongoose.connect(
-    `mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`, 
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }
+  `mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
 );
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error: "));
 db.once("open", function () {
-    console.log(`Database Connected successfully`.bold.blue);
-}); 
+  console.log(`Database Connected successfully`.bold.blue);
+});
 
 module.exports = db;
