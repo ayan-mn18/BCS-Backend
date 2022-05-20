365d6627a205b6b3771984459301f6be04f66a07
svnitad <svnitad@gmail.com>
2022-05-21 00:05:26 +0530

    reviews

--- Routes/user.routes.js
+++ Routes/user.routes.js
@@ -12,7 +12,7 @@ const router = express.Router();
 
 router.get("/alluser", isAuthenticated, is_Admin, allUser);
 router.get("/fetchuser", isAuthenticated, getUser);
-router.patch("/updateuser/:uid", isAuthenticated, is_Admin, updateUser);
+router.patch("/updateuser/:uid", isAuthenticated, updateUser);
 router.delete("/deleteuser/:uid", isAuthenticated, is_Admin, deleteUser);
 
 module.exports = router;
